68067312291b8ce7e2ab1280c0ccb9b65053eb2c
Bowen Brooks <bowenjbrooks@gmail.com>
2024-01-17 07:59:27 -0700

    Inital app commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusti = Lusitana({
+  weight: '700',
+  subsets: ['latin'],
+});
